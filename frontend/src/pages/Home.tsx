d54a81e863b3eecaa3a4ebba23693b8d6793e1a5
Raissa Vieira <raissa.vieira@lavid.ufpb.br>
2022-03-14 21:12:38 -0300

    feat: Pagina inicial da aplicacao

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,21 @@
+import { CardsProfessional } from '../components/CardsProfissional';
+import '../styles/home.css';
+
+export function Home() {
+    return (
+        <div id="home">
+            <header>
+                Estes são os profissionais disponíveis
+            </header>
+            <button className="create-profissional">Cadastrar profissional</button>
+            {/* <div className="no-profissional">
+                Nenhum profissional cadastrado até
+                <br/> o momento.
+            </div> */}
+
+            <main className="list-profissional">
+                <CardsProfessional/>
+            </main>
+        </div>
+    );
+}
